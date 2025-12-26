c17be270c3e43a6a96d2c1a72d42e55aa0e78a46
bhasker-sharma <bhaskersharma13@gmail.com.com>
2025-12-26 22:21:35 +0530

    name changes made

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar({ onConfigureClick, onPyrometerSettingsClick }) {
                     <div className="flex items-center space-x-3">
                         <Link to="/" className="flex items-center space-x-2 hover:opacity-80 transition">
                             <img src="/logo.png" alt="TIPL Logo" className="w-8 h-8 object-contain" />
-                            <span className="text-xl font-bold text-gray-800">Multi IR Thermometer</span>
+                            <span className="text-xl font-bold text-gray-800">Toshniwal Industries Pvt. Ltd. - Multi Infrared Thermometer</span>
                         </Link>
                     </div>
 
